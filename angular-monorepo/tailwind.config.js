8c113c37d831f6ef182c4d3a39889e065d76ae08
Matt <matthewnelson@Matts-MacBook-Pro-2.local>
2024-10-30 11:39:41 -0500

    add tailwind

---
+++ angular-monorepo/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./apps/**/*.{html,js,ts}",
+    "./libs/**/*.{html,js,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
