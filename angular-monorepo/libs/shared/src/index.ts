40530bb657508168d9032f593d18480d5e25d1b5
Matt Nelson <matt.dav.nelson@gmail.com>
2024-10-30 14:22:15 -0500

    add app container component

--- angular-monorepo/libs/shared/src/index.ts
+++ angular-monorepo/libs/shared/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/home-page/home-page.component'
+export * from './lib/app-container/app-container.component'
---
+++ angular-monorepo/libs/shared/src/lib/app-container/app-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-app-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-container.component.html',
+  styleUrl: './app-container.component.scss'
+})
+export class AppContainerComponent {
+
+}
