ae8d8a9d2d2dd519304379f372a4ed0b68805e8a
Matt <matt.dav.nelson@gmail.com>
2024-11-08 09:45:03 -0800

    feat:figure-out-env (#4)
    * at least I can do this
    
    * finally

---
+++ libs/environments/src/lib/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+    PROD: true,
+    API_URL: 'https://mono-be-7afa152d0ce3.herokuapp.com/'
+}
---
+++ libs/environments/src/lib/environments/environment.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+    PROD: false,
+    API_URL: 'http://localhost:3000'
+}
