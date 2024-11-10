675cd7016b6dd5d7b866b2ee3cdf48aa46f37622
Matt Nelson <matt.dav.nelson@gmail.com>
2024-11-10 10:53:17 -0600

    lil typo

--- libs/environments/src/lib/environments/environment.prod.ts
+++ libs/environments/src/lib/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { ENVIRONMENT } from "./environment.model";
 
 export const ENV: ENVIRONMENT = {
     PROD: true,
-    API_URL: 'https://mono-be-7afa152d0ce3.herokuapp.com/'
+    API_URL: 'https://mono-be-7afa152d0ce3.herokuapp.com'
 }
