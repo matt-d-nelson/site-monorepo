de02c4022fc6312ce2205e2f2452f36345842519
Matt Nelson <matt.dav.nelson@gmail.com>
2024-12-16 10:06:24 -0600

    flesh out service

---
+++ libs/shared-services/src/lib/videos-service/videos-service.service.ts
@@ -0,0 +1,46 @@
+import { ENV } from '@angular-monorepo/environments';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BehaviorSubject, finalize } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VideosServiceService {
+  constructor(
+    private http: HttpClient,
+    private spinnerService: NgxSpinnerService
+  ) { }
+
+  _videosCache = new BehaviorSubject<any[]>([])
+  videos$ = this._videosCache.asObservable()
+
+  createVideo(orgId: string, body: {}) {
+    return this.http.post(`${ENV.API_URL}/api/videos/${orgId}`, body)
+  }
+
+  getVideos(orgId: string) {
+    this.spinnerService.show()
+    this.http
+      .get(`${ENV.API_URL}/api/videos/${orgId}`)
+      .pipe(finalize(() => this.spinnerService.hide()))
+      .subscribe((videos: any) => {
+        this._videosCache.next(videos)
+      })
+  }
+
+  deleteVideo(orgId: string, videoId: string) {
+    this.spinnerService.show()
+    return this.http
+      .delete(`${ENV.API_URL}/api/videos/${orgId}/${videoId}`)
+      .pipe(finalize(() => this.spinnerService.hide()))
+  }
+
+  updateVideo(orgId: string, videoId: string, updatedData: any) {
+    return this.http.patch(
+      `${ENV.API_URL}/api/videos/${orgId}/${videoId}`,
+      updatedData
+    )
+  }
+}
