ff48767994894b78f32484c9542b43fca2758c69
Matt Nelson <matt.dav.nelson@gmail.com>
2025-01-04 11:42:18 -0600

    fix last tests

--- libs/shared-pages/src/lib/videos-page/videos-page.component.spec.ts
+++ libs/shared-pages/src/lib/videos-page/videos-page.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { VideosPageComponent } from './videos-page.component'
+import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClientTesting } from '@angular/common/http/testing'
+import {
+  AboutService,
+  AuthService,
+  OrgService,
+  VideosService,
+} from '@angular-monorepo/shared-services'
+import { createSpyFromClass } from 'jest-auto-spies'
 
 describe('VideosPageComponent', () => {
   let component: VideosPageComponent
@@ -9,10 +18,34 @@ describe('VideosPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [VideosPageComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: VideosService,
+          useValue: createSpyFromClass(VideosService),
+        },
+        {
+          provide: OrgService,
+          useValue: createSpyFromClass(OrgService, {
+            observablePropsToSpyOn: ['currentOrgId$'],
+          }),
+        },
+        { provide: AuthService, useValue: createSpyFromClass(AuthService) },
+      ],
     }).compileComponents()
 
     fixture = TestBed.createComponent(VideosPageComponent)
     component = fixture.componentInstance
+    const dialogElement = document.createElement('dialog')
+    if (!dialogElement.showModal) {
+      HTMLDialogElement.prototype.showModal = function () {
+        this.setAttribute('open', '')
+      }
+      HTMLDialogElement.prototype.close = function () {
+        this.removeAttribute('open')
+      }
+    }
     fixture.detectChanges()
   })
 
