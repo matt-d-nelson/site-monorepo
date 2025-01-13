3c5c3eafb3645d1b7e41da548d948e917e8b1197
Matt Nelson <matt.dav.nelson@gmail.com>
2025-01-13 14:32:20 -0600

    fix test

--- apps/matt-dav-nelson/src/app/app.component.spec.ts
+++ apps/matt-dav-nelson/src/app/app.component.spec.ts
@@ -1,26 +1,21 @@
-import { TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { AppComponent } from './app.component'
 import { RouterModule } from '@angular/router'
 
 describe('AppComponent', () => {
+  let component: AppComponent
+  let fixture: ComponentFixture<AppComponent>
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent, RouterModule.forRoot([])],
     }).compileComponents()
+
+    fixture = TestBed.createComponent(AppComponent)
+    component = fixture.componentInstance
   })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    fixture.detectChanges()
-    const compiled = fixture.nativeElement as HTMLElement
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome matt-dav-nelson'
-    )
-  })
-
-  it(`should have as title 'matt-dav-nelson'`, () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app.title).toEqual('matt-dav-nelson')
+  it('should create', () => {
+    expect(component).toBeTruthy()
   })
 })
