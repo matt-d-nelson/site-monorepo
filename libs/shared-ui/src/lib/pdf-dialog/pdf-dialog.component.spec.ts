90163808d29eb5bcd6117af3107d34a1c958d194
Matt Nelson <matt.dav.nelson@gmail.com>
2025-01-16 14:41:36 -0600

    fix tests

--- libs/shared-ui/src/lib/pdf-dialog/pdf-dialog.component.spec.ts
+++ libs/shared-ui/src/lib/pdf-dialog/pdf-dialog.component.spec.ts
@@ -1,9 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { PdfDialogComponent } from './pdf-dialog.component'
+import { ComponentRef } from '@angular/core'
 
 describe('PdfDialogComponent', () => {
   let component: PdfDialogComponent
+  let componentRef: ComponentRef<PdfDialogComponent>
   let fixture: ComponentFixture<PdfDialogComponent>
 
   beforeEach(async () => {
@@ -13,6 +15,19 @@ describe('PdfDialogComponent', () => {
 
     fixture = TestBed.createComponent(PdfDialogComponent)
     component = fixture.componentInstance
+    componentRef = fixture.componentRef
+
+    const dialogElement = document.createElement('dialog')
+    if (!dialogElement.showModal) {
+      HTMLDialogElement.prototype.showModal = function () {
+        this.setAttribute('open', '')
+      }
+      HTMLDialogElement.prototype.close = function () {
+        this.removeAttribute('open')
+      }
+    }
+
+    componentRef.setInput('config', {})
     fixture.detectChanges()
   })
 
